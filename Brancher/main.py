15494f6549e6195d2bc75fd0022d816ce91ed311
InEase <InEase28@gmail.com>
2022-03-24 00:50:11 +0800

    feat(core): before hook

--- Brancher/main.py
+++ Brancher/main.py
@@ -13,7 +13,6 @@ def Plugin(cls):
 
     # Make a new definition
     def run_with_hook(self, name):
-        print(name)
 
         for b in before_hook[name]:
             b(self)
@@ -33,14 +32,23 @@ def Plugin(cls):
 
 def get_class_that_defined_method(meth):
     if inspect.ismethod(meth):
-        print('this is a method')
         for cls in inspect.getmro(meth.__self__.__class__):
             if meth.__name__ in cls.__dict__:
                 return cls
     if inspect.isfunction(meth):
-        print('this is a function')
         return getattr(inspect.getmodule(meth),
                        meth.__qualname__.split('.<locals>', 1)[0].rsplit('.', 1)[0],
                        None)
-    print('this is neither a function nor a method')
     return None  # not required since None would have been implicitly returned anyway
+
+
+def before(*args, **kwargs):
+    def inner(hook):
+        for target in args:
+            cls = get_class_that_defined_method(target)
+            cls.__before_hook__[target.__name__].append(hook)
+
+        return hook
+
+    # returning inner function
+    return inner
